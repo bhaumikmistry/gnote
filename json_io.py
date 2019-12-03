9fa2b3e4b7e95a77a0d48358c0f8bfe7405e48b2
Bhaumik Mistry <bhaumikpmistry@gmail.com>
2019-12-03 13:22:39 -0500

    added save read feature in the create

--- json_io.py
+++ json_io.py
@@ -10,23 +10,22 @@ class constants:
     DATA="data"
     NAME="name"
     TAG="tag"
-
-empty_tag = '{\
+    empty_tag = '{\
         "tag":"",\
         "content":[]\
         }'
-        
-empty_notes_holder = [{\
-    "name":"",\
-    "notes":[]\
-    }]
+            
+    empty_notes_holder = [{\
+        "name":"",\
+        "notes":[]\
+        }]
 
-empty_note = [{\
-    "created":"some_date",\
-    "edited":"last_date",\
-    "revised":"last_revised",\
-    "data":""\
-    }]
+    empty_note = [{\
+        "created":"some_date",\
+        "edited":"last_date",\
+        "revised":"last_revised",\
+        "data":""\
+        }]
     
 class json_io:
     
@@ -41,26 +40,32 @@ class json_io:
         self._if_json_exists = os.path.exists(self._json_name)       
 
     def __create_empty_json_note(self):
-        tag_file = json.loads(empty_tag)
-        tag_file[constants.CONTENT]+=empty_notes_holder
+        # create empty tag file
+        tag_file = json.loads(constants.empty_tag)
+        # add empty note to note structure notes list 
+        tag_file[constants.CONTENT]+=constants.empty_notes_holder
+        # add empty single note structure to notes list
         for notes in tag_file[constants.CONTENT]:
-            notes[constants.NOTES]+=empty_note
+            notes[constants.NOTES]+=constants.empty_note
         return tag_file
 
     def create(self):
+        # check of the note with tag name exists
         if not self._if_json_exists:
+            # add tag and note name 
             empty_file = self.__create_empty_json_note()
-            print(empty_file)
             empty_file[constants.TAG]=self._tag
             for name in empty_file[constants.CONTENT]:
-                print(name)
                 if name[constants.NAME] == '':
                     name[constants.NAME] = self._name
             print(empty_file)
-
+            self.__save(empty_file)
+        else:
+            print(f"{self._json_name} already present")
         
-    def save(self):
-        pass    
+    def __save(self,data_to_save):
+        with open(self._json_name,'w') as jsonfile:
+            json.dump(data_to_save, jsonfile, indent=4)
 
     def delete(self):
         pass
