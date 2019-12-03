43bdb4431efba473811ea7620f00df982393b1c7
Bhaumik Mistry <bhaumikpmistry@gmail.com>
2019-12-03 13:07:52 -0500

    json file interface test phase to create update and manage notes with tags inside

---
+++ json_io.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+class constants:
+    NOTES="notes"
+    CONTENT="content"
+    CREATED="created"
+    EDITED="edited"
+    REVISED="revised"
+    DATA="data"
+    NAME="name"
+    TAG="tag"
+
+empty_tag = '{\
+        "tag":"",\
+        "content":[]\
+        }'
+        
+empty_notes_holder = [{\
+    "name":"",\
+    "notes":[]\
+    }]
+
+empty_note = [{\
+    "created":"some_date",\
+    "edited":"last_date",\
+    "revised":"last_revised",\
+    "data":""\
+    }]
+    
+class json_io:
+    
+    def __init__(self,tag,name):
+        self._tag = tag
+        self._name = name
+        self._json_name = "%s.json" % self._tag
+        self.__is_json_file_present()
+        pass
+
+    def __is_json_file_present(self):
+        self._if_json_exists = os.path.exists(self._json_name)       
+
+    def __create_empty_json_note(self):
+        tag_file = json.loads(empty_tag)
+        tag_file[constants.CONTENT]+=empty_notes_holder
+        for notes in tag_file[constants.CONTENT]:
+            notes[constants.NOTES]+=empty_note
+        return tag_file
+
+    def create(self):
+        if not self._if_json_exists:
+            empty_file = self.__create_empty_json_note()
+            print(empty_file)
+            empty_file[constants.TAG]=self._tag
+            for name in empty_file[constants.CONTENT]:
+                print(name)
+                if name[constants.NAME] == '':
+                    name[constants.NAME] = self._name
+            print(empty_file)
+
+        
+    def save(self):
+        pass    
+
+    def delete(self):
+        pass
+
+    def edit(self):
+        pass
+
+if __name__ == "__main__":
+    jio = json_io("cpp","test")
+    jio.create()
