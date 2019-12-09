7ad59ff5a7a417744b95eb7804f87479a7355d7c
bhaumikmistry <bhaumikpmistry@gmail.com>
2019-12-09 12:03:24 -0500

    add new display class

---
+++ display.py
@@ -0,0 +1,16 @@
+
+
+
+
+class display:
+    def __init__(self):
+        pass
+
+    def list_tag(self):
+        pass
+
+    def list_tag_note_name(self):
+        pass
+
+    def list_tag_note_name_with_data(self):
+        pass
